150178c6438a9e2aa03912427356911549d33e56
Mike Boyle <mikesboyle@gmail.com>
2023-02-07 12:22:10 -0500

    connect backend to deployed db

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 // Import OUR stuff (our files, our components)
 const studentsController = require('./controllers/studentsController');
 const studentsControllerV2 = require('./controllers/v2/studentsControllerV2');
+const db = require('./db');
 
 // Init express application
 const app = express();
@@ -27,6 +28,16 @@ app.use('/v2/students', studentsControllerV2);
 // Healthcheck route
 app.get('/', (request, response) => {
   response.status(200).json({ data: 'Service is running' });
+});
+
+app.get('/tests', async (request, response) => {
+  try {
+    const tests = await db.any('SELECT * FROM tests');
+
+    response.status(200).json({ data: tests });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
 });
 
 
